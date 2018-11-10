4f4f4bb32c9cbe0cfdbfa203de42125e9ab804ad
lk <2269784144@qq.com>
2018-11-10 21:03:52 +0800

    login

---
+++ future/login/models/authimages.py
@@ -0,0 +1,4 @@
+import random
+from PIL import (Image,ImageDraw,ImageFont,ImageFilter)
+
+def check_code(width=80,height=30,char_length=5, font_file='kumo.ttf')
--- future/login/views.py
+++ future/login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from PIL import Image,ImageDraw
+from io import BytesIO
 
 # Create your views here.
 def auth(request):
@@ -9,3 +10,18 @@ def auth(request):
         user = request.POST.get('us')
         password = request.POST.get('pa')
         return render(request, 'login/index.html', {'users': user, 'passwords': password})
+def checkcode(request):
+    f = BytesIO()
+    img = Image.new(mode="RGB", size=(100, 100), color=(80, 80, 80))
+    draw = ImageDraw.Draw(img,mode='RGB')
+    from PIL import ImageFont
+    font = ImageFont.truetype("kumo.ttf",25)
+    import random
+    code_list =[]
+    for i in range(5):
+        char = chr(random.randint(60,60))
+        code_list.append(char)
+        draw.text((i*20, 10),char,fill=(random.randint(0,255),random.randint(0,255),random.randint(0,255)),font = font)
+        img.save(f,'png')
+        data = f.getvalue()
+        return HttpResponse()
