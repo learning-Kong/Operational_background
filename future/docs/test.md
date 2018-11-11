9a6e44194c36f99c8114d408634bd271324aeda8
test <2269784144@qq.com>
2018-11-11 22:19:09 +0800

    login

--- future/docs/test.md
+++ future/docs/test.md
@@ -1,1 +1,2 @@
 https://www.cnblogs.com/liuzhipenglove/p/8012004.html
+http://www.cssmoban.com/cssthemes/5870.shtml
--- future/login/views.py
+++ future/login/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
-from PIL import Image,ImageDraw
-from io import BytesIO
+from django.http import HttpResponse
 
 # Create your views here.
 def auth(request):
     if request.method == 'GET':
         return render(request, 'login/login.html')
     elif request.method == 'POST':
-        user = request.POST.get('us')
-        password = request.POST.get('pa')
-        return render(request, 'login/index.html', {'users': user, 'passwords': password})
+        user = request.POST.get('user')
+        password = request.POST.get('password')
+        return render(request, 'login/index.html', {'users': user, 'passwords': password, 'session':request.session.get('code')})
 def checkout(request):
         from io import BytesIO
-        from utils.checkCode import check_code
+        from .models.authimages import check_code
         img, code = check_code()
         stream = BytesIO()
         img.save(stream, 'png')
