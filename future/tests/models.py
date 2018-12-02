3056d69cc6127dfafe54501dc458a8202dc1de39
test <2269784144@qq.com>
2018-12-02 21:48:37 +0800

    login

--- future/tests/models.py
+++ future/tests/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Person(models.Model):
+class User(models.Model):
+    userid = models.Charfield(max_length=30 , primary_key = True)
     username = models.Charfield(max_length=30)
+    password = models.Charfield(max_length=30)
+    email = models.EmailField()
+
+class Group(models.Model):
+    groupid = models.Charfield(max_length=30, primary_key = True)
+    userid = models.Charfield(max_length=30)
+    department = models.Charfield(max_length=30)
+
+class authorityinfo(models.Model):
+    authorityid = models.Charfield(max_length=30, primary_key = True)
+    authorityname = models.Charfield(max_length=30)
+
+class authority(models.Model):
+    userid = models.Charfield(max_length=30)
+    groupid = models.Charfield(max_length=30)
+    authorityid = models.Charfield(max_length=30)
