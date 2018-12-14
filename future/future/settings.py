a7a614a6782cbe5e2cfcad2adf1c56e7c4019e71
Administrator <admin@example.com>
2018-12-14 10:40:18 +0800

    change settings.py ALLOW_HOSTS

--- future/future/settings.py
+++ future/future/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k63cnnl(4uf&cwyf^=e0yv(wz0ni(%-guyd^$5y)a)1o#k6hu2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
