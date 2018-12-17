98f3376ba155b03ad4f629a0abd4e78762d29b4b
Administrator <admin@example.com>
2018-12-17 17:47:35 +0800

    uwsgi

--- future/login/views.py
+++ future/login/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def login(request):
-    return HttpResponse('Hello word!')
+    return HttpResponse('Hello word! nice')
