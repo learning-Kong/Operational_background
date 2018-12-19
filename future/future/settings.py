15794530e77e02f4725d2fe83518b480b6e8a2c4
Administrator <admin@example.com>
2018-12-19 11:27:18 +0800

    login-ajax

--- future/future/settings.py
+++ future/future/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'future.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'future',
+        'USER': 'django',
+        'PASSWORD': 'django123',
+        'HOST': '115.159.40.122',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- future/login/views.py
+++ future/login/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render,HttpResponse,HttpResponseRedirect
+from django.shortcuts import render,HttpResponse
+import json
+
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'login/login.html')
+    if request.method == 'GET':
+        return render(request, 'login/login.html')
+    if request.method == "POST":
+        # print(request.method.data)
+        data = {'status': False, 'message': 'nice to meet you'}
+        return HttpResponse(json.dumps(data))
 
-# def log_302(request):
-#     return HttpResponseRedirect('/login/')
