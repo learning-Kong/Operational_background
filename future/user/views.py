6e9ac2227835cbd327e140b153bf51fcf73eb435
Administrator <admin@example.com>
2018-12-21 18:18:05 +0800

    login-title

--- future/user/views.py
+++ future/user/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def add(request):
-    return HttpResponse('ok')
+    return render(request, 'user/user_add.html')
 
 def change(request):
     return HttpResponse('change')
