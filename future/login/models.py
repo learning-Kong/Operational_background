bc8c4190a7e76f3ddbc6faadd2dde9c85204ed50
Administrator <admin@example.com>
2018-12-24 18:23:56 +0800

    login-title

--- future/login/models.py
+++ future/login/models.py
@@ -4,14 +4,13 @@ from django.db import models
 
 class UserGroup(models.Model):
     department = models.CharField(max_length=256)
-    position_id = models.IntegerField(default=1)
     position = models.CharField(max_length=256)
 
 class User(models.Model):
     gender = (('male', '男'), ('female', '女'))
     name = models.CharField(max_length=123, unique=True)
     password = models.CharField(max_length=256)
-    email = models.EmailField(unique=True)
+    email = models.EmailField()
     tel = models.CharField(max_length=11, default=None)
     sex = models.CharField(max_length=32, choices=gender, default='男')
     create_time = models.DateTimeField(auto_now_add=True)
--- future/user/views.py
+++ future/user/views.py
@@ -1,13 +1,50 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from future.login_certification import auth
+import json
+import re
+from login import models
+
+
 # Create your views here.
 
 @auth
 def add(request):
     if request.method == "GET":
-        return render(request, 'user/user_add.html')
+        Groupinfo = models.UserGroup.objects.filter()
+        return render(request, 'user/user_add.html', {'Groupinfo': Groupinfo})
     if request.method == "POST":
-        print('OK')
+        pwd_null = '01b1f2dd8ba23c813b13a2e2e55f8af457b0f9daa925ec67bf16d7348c11a25c'
+        data = {'status': False, 'message': 'nice to meet you'}
+        User_name = request.POST.get('username')
+        User_tel = request.POST.get('tel')
+        User_email = request.POST.get('email')
+        User_password = request.POST.get('pwd')
+        User_re_password = request.POST.get('re_pwd')
+        Gid = request.POST.get('value')
+        pwd_db = models.User.objects.filter(name=User_name).first()
+        if not pwd_db:
+            if User_name != '':
+                if re.fullmatch(r"1\d{10}", User_tel):
+                    if re.match(r'^[0-9a-zA-Z_]{0,19}@[0-9a-zA-Z]{1,13}\.[com,cn,net]{1,3}$', User_email):
+                        if User_password == User_re_password:
+                            if User_password != pwd_null:
+                                models.User.objects.create(name=User_name, password=User_password, email=User_email, tel=User_tel, G_id = Gid)
+                                data['status'] = True
+                            else:
+                                data['message'] = '密码不能为空'
+                        else:
+                            data['message'] = '两次输入的密码不一样'
+                    else:
+                        data['message'] = '输入的Emil格式不正确'
+                else:
+                    data['message'] = '输入的电话号码不正确'
+            else:
+                data['message'] = '用户名不能为空'
+        else:
+            data['message'] = '用户名已存在'
+        return HttpResponse(json.dumps(data))
 
+@auth
 def change(request):
-    return HttpResponse('change')
+    if request.method == "GET":
+        return render(request, 'user/user_change.html')
