d57baab3699436350676bdb10227ac2eb92fbf9c
Administrator <admin@example.com>
2019-01-09 17:38:17 +0800

    login-title

--- future/assets/form.py
+++ future/assets/form.py
@@ -6,5 +6,5 @@ class IDC_form(forms.Form):
     idc_area = forms.CharField(label='机房地址', max_length=128,  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '机房地址'}))
     idc_operator = forms.ChoiceField(label="运营商", choices=idc_operator, widget=forms.Select(attrs={'class': 'form-control select2 select2-hidden-accessible', 'style': 'width: 100%;', 'tabindex': '-1', 'ria-hidden': 'true'}))
     idc_type = forms.ChoiceField(label="机房类型", choices=idc_type,  widget=forms.Select(attrs={'class': 'form-control select2 select2-hidden-accessible', 'style': 'width: 100%;', 'tabindex': '-1', 'ria-hidden': 'true'}))
-    idc_comment = forms.CharField(label="备注", required=False, widget=forms.Textarea(attrs={'class': 'textarea form-control', 'style': 'width: 50%;'}))
+    idc_comment = forms.CharField(label="备注", required=False, widget=forms.Textarea(attrs={'class': 'textarea form-control', 'style': 'width: 100%;'}))
 
