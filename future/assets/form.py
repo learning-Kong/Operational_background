5ce9c4b2cb6f8edd0085a9c35ba75beee1c134b9
Administrator <admin@example.com>
2019-01-10 16:30:11 +0800

    login-title

--- future/assets/form.py
+++ future/assets/form.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import idc_operator, idc_type
 
 class IDC_form(forms.Form):
+    idc_id = forms.IntegerField(initial=0, widget=forms.TextInput(attrs={'class': 'hide'}))
     idc_name = forms.CharField(label='机房名称', max_length=64,  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '机房名称'}))
     idc_area = forms.CharField(label='机房地址', max_length=128,  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '机房地址'}))
     idc_operator = forms.ChoiceField(label="运营商", choices=idc_operator, widget=forms.Select(attrs={'class': 'form-control select2 select2-hidden-accessible', 'style': 'width: 100%;', 'tabindex': '-1', 'ria-hidden': 'true'}))
--- future/assets/views.py
+++ future/assets/views.py
@@ -15,21 +15,50 @@ def idc_add(request):
             idc_operator = request.POST.get('idc_operator')
             idc_type = request.POST.get('idc_type')
             idc_comment = request.POST.get('idc_comment')
-            print(idc_name, idc_area, idc_operator, idc_type, idc_comment)
-            models.IDC.objects.create(name=idc_name, address=idc_area, operator=idc_operator, type=idc_type, comment=idc_comment)
+            idc_id = request.POST.get('idc_id')
+            print(idc_name, idc_area, idc_operator, idc_type, idc_comment, idc_id)
+            if int(idc_id) == 0:
+                models.IDC.objects.create(name=idc_name, address=idc_area, operator=idc_operator, type=idc_type, comment=idc_comment)
+                # models.IDC.objects.create(**obj.cleaned_data)
+            else:
+                models.IDC.objects.filter(id=idc_id).update(name=idc_name, address=idc_area, operator=idc_operator, type=idc_type, comment=idc_comment)
             return redirect('/assets/idc_list/')
+        return HttpResponse('请求错误')
     else:
         idc_form = IDC_form()
         return render(request, 'assets/idc_add.html', {'idc_form': idc_form})
+
+@auth
 def idc_list(request):
     if request.method == "GET":
         current_page = int(request.GET.get('p', 1))  # 获取当前页码
         num = models.IDC.objects.all().count()  # 数据库数据个数
-        per_page = 2  # 每页显示个数
+        per_page = 10  # 每页显示个数
         page_num = 7  # 页数标签个数
-
         page_obj = paging.Page(current_page, num, per_page, page_num)
         idc_info = models.IDC.objects.filter()[page_obj.start():page_obj.end()]
         page_str = page_obj.page_str()
         # idc_info = models.IDC.objects.filter()
         return render(request, 'assets/idc_list.html', {'idc_info': idc_info, 'page_str': page_str})
+
+@auth
+def idc_change(request):
+    if request.method == "GET":
+        idc_id = int(request.GET.get('idc_id', 0)) #获取需要修改页面的值，默认为0
+        idc_status = models.IDC.objects.filter(id=idc_id).first()
+        if not idc_status:
+            return HttpResponse('需要修改的主机不存在')
+        else:
+            idc_form = IDC_form(initial={'idc_id': idc_status.id, 'idc_name': idc_status.name, 'idc_area': idc_status.address, 'idc_comment': idc_status.comment, 'idc_operator': idc_status.operator, 'idc_type': idc_status.type})
+            return render(request, "assets/idc_change.html", {"idc_form": idc_form})
+
+@auth
+def idc_del(request):
+    if request.method == "GET":
+        print(1)
+        idc_id = int(request.GET.get('idc_id', 0))
+        if idc_id == 0:
+            return HttpResponse('需要修改的主机不存在')
+        else:
+            models.IDC.objects.filter(id=idc_id).delete()
+            return redirect('/assets/idc_list/')
