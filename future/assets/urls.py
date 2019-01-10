68b825928ba36d0673525fb8e7510bff3bde64d1
Administrator <admin@example.com>
2019-01-10 16:29:14 +0800

    IDC管理

--- future/assets/urls.py
+++ future/assets/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('idc_list/', views.idc_list),
     path('idc_add/', views.idc_add),
+    path('idc_change/', views.idc_change),
+    path('idc_del/', views.idc_del),
 ]
