80c0fd7aee1e036cfd8b54e5afb6fded729e8b6d
test <2269784144@qq.com>
2019-01-14 22:54:10 +0800

    chart

--- future/display/urls.py
+++ future/display/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('index/', views.index),
     path('login/',views.login),
     path('chart/',views.chartdisplay),
+    path('charttest/',views.chartss),
 ]
--- future/display/views.py
+++ future/display/views.py
@@ -2,6 +2,9 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 from future import config
+import pandas as pd
+import numpy as np
+from numpy import nan as NaN
 import time
 import json
 
@@ -23,7 +26,7 @@ def login(request):
         return HttpResponse(r.text)
 def chartdisplay(request):
     t = int(time.time())
-    params= {"step": 60,"start_time": (t-1000),"hostnames": ["test"],"end_time": t-100,"counters": ["cpu.idle"],"consol_fun": "AVERAGE"}
+    params= {"step": 60,"start_time": 1547461220,"hostnames": ["test"],"end_time": 1547461320,"counters": ["cpu.idle"],"consol_fun": "AVERAGE"}
     head = {'Apitoken':json.dumps({"name":"root","sig":"e55a622decc811e8aef5000c2913d5c4"}),'X-Forwarded-For':'192.168.1.190',"Content-Type": "application/json"}
     print('\n','aaaaaaaaaaaa')
     r = requests.post("%s/graph/history" % (config.API_ADDR), data=json.dumps(params), headers=head)
@@ -31,13 +34,20 @@ def chartdisplay(request):
     print(a[0]['Values'])
     tlist=[]
     vlist=[]
-    for x in a[0]['Values']:
-        tlist.append(x['timestamp'])
-        vlist.append(x['value'])
-        print(x['timestamp'])
-        print(x['value'])
-    print(r.text,'\n',a[0]['Values'])
-    print(tlist,vlist)
+    b=a[0]['Values']
+    test= pd.DataFrame(b)
+    print(test,type(test))
+    # for x in test:
+    #     tlist.append(x['timestamp'])
+    #
+    #     vlist.append( x['value'])
+    #     print(x['timestamp'])
+    #     print(x['value'])
+    #print(r.text,'\n',a[0]['Values'])
+    #print(tlist,vlist)
     print(int(t))
     #return HttpResponse(r.text)
-    return render(request, 'display/chart.html',{'tlist': tlist,'vlist':vlist})
+    #return render(request, 'display/chart.html',{'tlist': tlist,'vlist':vlist})
+    return render(request, 'display/chart.html',{'tlist': test.timestamp,'vlist':test.value})
+def chartss(request):
+    return render(request, 'display/chart test.html')
