909b48ae8116f12559ce826dc0e93d2c11baee48
lk <2269784144@qq.com>
2019-03-06 10:42:01 +0800

    g2

--- future/display/views.py
+++ future/display/views.py
@@ -16,7 +16,7 @@ def index(request):
 
 def login(request):
     if request.method == 'GET':
-        params = {'name': 'root', "password": 'Aa123456', }
+        params = {'name': 'lktest', "password": 'Aa123456', }
         head = {"Content-type": "application/json"}
         r = requests.post("%s/user/login" % (config.API_ADDR), data=json.dumps(params), headers=head)
         print(request.session)
@@ -26,8 +26,8 @@ def login(request):
         return HttpResponse(r.text)
 def chartdisplay(request):
     t = int(time.time())
-    params= {"step": 60,"start_time": 1548112805,"hostnames": ["localhost.localdomain"],"end_time": 1549894805,"counters": ["cpu.idle"],"consol_fun": "AVERAGE"}
-    head = {'Apitoken':json.dumps({"name":"root","sig":"13953a520ff211e9917686bf05b23eb4"}),'X-Forwarded-For':'192.168.15.156',"Content-Type": "application/json"}
+    params= {"step": 60,"start_time": 1551711152,"hostnames": ["VM_72_46_centos"],"end_time": 1551712152,"counters": ["cpu.idle"],"consol_fun": "AVERAGE"}
+    head = {'Apitoken':json.dumps({"name":"lktest","sig":"268656f33e5c11e9b8ad525400c86780"}),'X-Forwarded-For':'192.168.15.156',"Content-Type": "application/json"}
     print('\n','aaaaaaaaaaaa')
     r = requests.post("%s/graph/history" % (config.API_ADDR), data=json.dumps(params), headers=head)
     a=r.json()
